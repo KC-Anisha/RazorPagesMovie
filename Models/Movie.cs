6bb71d0a97a33f7259b03c88417f9292337de31a
Anisha KC <anisha.kc7219@gmail.com>
2022-04-04 21:05:55 -0400

    Added validation

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
@@ -6,14 +7,30 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int Id { get; set; }
+
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; } = string.Empty;
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        [Required]
         public DateTime ReleaseDate { get; set; }
+
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
         public string Genre { get; set; } = string.Empty;
 
-        [Column(TypeName = "decimal(18,2)")]
+
+        [Range(1, 100), DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        [Required]
         public decimal Price { get; set; }
+
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
+        [Required]
+        public string Rating { get; set; } = string.Empty;
     }
 }
